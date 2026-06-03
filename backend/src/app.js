fbc903477a4040573912d9c57924cc34730bf6e3
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2026-06-03 17:43:24 +0600

    implement register endpoint and created status constant

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import {appRouter} from "./routes/appRoute.js";
+
 
 
 export const app = express()
+
+app.use(express.json({limit: '16kb'}))
+app.use('/api/v1', appRouter)
