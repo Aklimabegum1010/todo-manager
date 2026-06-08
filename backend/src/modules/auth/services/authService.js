7cf6486691d3b1d4ac09d7a46995ceef0e53ba65
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2026-06-08 23:55:40 +0600

    feat: implement auth service registration and request body validation middleware

--- backend/src/modules/auth/services/authService.js
+++ backend/src/modules/auth/services/authService.js
@@ -1,16 +1,31 @@
 import {createAuthRepository} from '../repositories/authRepository.js';
+import {generateAccessToken} from "../../../utils/jwt.js";
 
 
 
 
 
 export const createAuthService = (userRepository = createAuthRepository()) => {
+
+
+const generateTokenPair = async (userId) => {
+const accessToken = generateAccessToken(userId)
+    return{
+    accessToken
+    }
+}
+
+
+
     return {
         register: async ({name, email, password}) => {
             const user = await userRepository.create({name, email, password})
+            const tokens = await generateTokenPair(user._id)
             return {
-                user
+                user,
+                ...tokens
             }
         }
     }
 }
+
