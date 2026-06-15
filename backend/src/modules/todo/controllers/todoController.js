833912ac08d64add48a5594a739193192392fd40
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2026-06-15 21:53:36 +0600

    feat: implement todo module with MVC pattern, validation, and database schema

--- backend/src/modules/todo/controllers/todoController.js
+++ backend/src/modules/todo/controllers/todoController.js
@@ -5,6 +5,7 @@ import {http_status} from "../../../shared/constants.js";
 
 export const createTodo = asyncHandler(async (req, res) => {
 const todo = await todoService.create(req.boby)
+    
     new ApiResponse(http_status.created, todo, 'todo created successfully')
         .send(res)
 
